6949f440a58026b8d468b4228beea131b5230cac
wangyi <foyewy@gmail.com>
2026-03-24 20:58:47 +0800

    ci: actions

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started123</h1>
+          <h1>Get started1234</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
